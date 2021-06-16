46a3594461d2bbfd6b130bd2508bf5ecb150d891
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 21:45:28 +0300

    Add files via upload

---
+++ week11,12/week11,12#4.cpp
@@ -0,0 +1,38 @@
+﻿/*
+  Робот-автомобиль описывается как структура с полями: скорость, длительность 
+движения и номер. Напишите функцию для расчёта пройденного расстояния. 
+*/
+
+#include <iostream>
+#include "fstream"
+
+using namespace std;
+
+struct parametr {
+    float speed;
+    float time;
+    float number;
+};
+
+float distance(parametr Auto);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    ofstream file;
+    file.open("Task_20.txt");
+
+    parametr pAuto;
+
+    cin >> pAuto.speed >> pAuto.time;
+    file << "Скорость автомобиля: " << pAuto.speed << endl;
+    file << "Длительность движения автомобиля: " << pAuto.time << endl;
+
+    file << "Пройденное расстояние: " << distance(pAuto) << endl;
+
+    file.close();
+}
+
+float distance (parametr Auto) {
+    return (Auto.speed * Auto.time);
+}

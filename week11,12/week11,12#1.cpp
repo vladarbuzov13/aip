be20b9172266f53b030865eef837a22fc9a28a0d
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 21:32:45 +0300

    Add files via upload

---
+++ week11,12/week11,12#1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+	ofstream file;
+
+	struct Point {
+
+		float y, x;
+
+	};
+
+	Point A;
+
+	cout << "Imput x,y:";
+	cin >> A.x >> A.y;
+
+	if ((A.y < sin(A.x)) and (A.y < 0.5)) {
+
+		file.open("file.txt");
+		file << "Da, popadaet";
+		file.close();
+
+		cout << "Da, popadaet";
+
+	}
+
+	else {
+
+		file.open("file.txt");
+		file << "Net, ne popadaet";
+		file.close();
+
+		cout << "Net, ne popadaet";
+
+
+	}
+
+	return(0);
+}

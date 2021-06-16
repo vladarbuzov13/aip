0104619579853c2959b7004ebf77d84534731215
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 22:04:32 +0300

    Add files via upload

---
+++ week15,16/week15,16#2.cpp
@@ -0,0 +1,40 @@
+﻿#include <ctime>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+const int rows = 3;  // ñòðîêè
+const int cols = 3;  // ñòîëáöû
+int mat[rows][cols];
+
+void pr() {
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+            mat[i][j] = mat[i][j] * mat[i][2];
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+    srand(time(0));
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            mat[i][j] = rand() % 10 + rand() % 100;
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    pr();
+}

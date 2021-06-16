8063e622366abf32155c2dde91c158b7a6846301
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 22:14:32 +0300

    Add files via upload

---
+++ week15,16/week15,16#9b.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+const int r = 4;
+const int c = 4;
+
+struct Shah {
+    float x, y;
+};
+
+Shah m[r][c];
+Shah pix;
+
+void matrix() {
+    cout << "Øàõìàòíàÿ äîñêà 4õ4:" << endl;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++)
+            cout << "x: " << m[i][j].x << "     ";
+        cout << endl;
+        for (int j = 0; j < c; j++)
+            cout << "y: " << m[i][j].y << "     ";
+        cout << endl;
+        cout << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "Ïóñòü øàõìàòíàÿ äîñêà 4õ4" << endl;
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++) {
+            cout << "Êëåòêà " << i + 1 << "." << j + 1 << endl;
+            cout << "Çàðÿä- ";
+            cin >> pix.x;
+            cout << "Ñêîðîñòü- ";
+            cin >> pix.y;
+            m[i][j] = pix;
+            cout << endl;
+        }
+    cout << endl;
+    matrix();
+}

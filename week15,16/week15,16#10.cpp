b1e62743bf9ffc29de326d6777937366e695f466
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 22:17:24 +0300

    Add files via upload

---
+++ week15,16/week15,16#10.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+
+
+const int r = 3;
+const int c = 3;
+int a[r][c];
+int b[r][c];
+int w[r][c];
+int d[r][c];
+int e[r][c];
+
+void sum() 
+{
+
+    cout << "A+B: " << endl;
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j < c; j++)
+            cout << "  " << a[i][j] + b[i][j];
+        cout << endl;
+    }
+    cout << endl;
+
+}
+
+void umnozh() 
+{
+
+    cout << "A*B: " << endl;
+
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j < c; j++) 
+        {
+            w[i][j] = 0;
+            for (int k = 0; k < r; k++)
+                w[i][j] += a[i][k] * b[k][j];
+            cout << "  " << w[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+}
+
+void minus()
+{
+
+    cout << "A-B: " << endl;
+
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j < c; j++)
+            cout << "  " << a[i][j] - b[i][j];
+        cout << endl;
+    }
+    cout << endl;
+
+}
+
+void trans_A() 
+{
+
+    cout << "trans A: " << endl;
+
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j < c; j++)
+            d[i][j] = a[j][i];
+    }
+
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j < c; j++)
+            cout << d[i][j] << " ";
+        cout << endl;
+    }
+    cout << endl;
+
+}
+
+void trans_B() 
+{
+
+    cout << "trans B: " << endl;
+
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j < c; j++)
+            e[i][j] = b[j][i];
+    }
+
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j < c; j++)
+            cout << e[i][j] << " ";
+        cout << endl;
+    }
+
+}
+
+int main() 
+{
+
+    cout << "Matrica A: " << endl;
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++) 
+        {
+            cin >> a[i][j];
+        }
+    cout << endl;
+
+    cout << "Matrcica B: " << endl;
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++) 
+        {
+            cin >> b[i][j];
+        }
+    cout << endl;
+
+    sum();
+    minus();
+    umnozh();
+    trans_A();
+    trans_B();
+
+    return 0;
+
+}

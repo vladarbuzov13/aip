f015d14985c4912fcdf395a6a72a6ba5ea5f5240
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 22:00:08 +0300

    Add files via upload

---
+++ week13,14/week13,14#4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+
+int main() {
+	setlocale(0, "");
+	const int n = 10;
+	int arr[n];
+
+	std::cout << "Íà÷àëüíûé ìàññèâ: ";
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 10 - 5;
+		std::cout << arr[i] << " ";
+	}
+
+	std::cout << "\nÈçìåíåííûé ìàññèâ èìååò âèä: ";
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < 0)
+			arr[i] = arr[i] * 5;
+		if (arr[i] > 0) 
+			arr[i] = arr[i] - 1;
+			std::cout << arr[i] << " ";
+	}
+}

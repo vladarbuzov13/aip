7fbdc6821e6d712a2e89cd1813bb3382b5613cb7
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 21:05:31 +0300

    Add files via upload

---
+++ week6,7/week6#2b.cpp
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает от пользователя координаты точки 
+// и определяет, попала ли точка в заштрихованную область
+// 2-В
+
+#include <iostream>
+#include <math.h>
+
+
+int main()
+{
+    float x, y;
+    std::cout << "Enter coordinate (x) then (y): " << std::endl;
+    std::cin >> x >> y;
+
+    if ((1 > (x * x + (y - 1) * (y - 1))) and (y < 1 - x * x))
+        std::cout << "Da, to4ka popadaet";
+    else
+        std::cout << "Net, to4ka ne popadaet";
+
+    std::getchar();
+    std::getchar();
+
+    return 0;
+}

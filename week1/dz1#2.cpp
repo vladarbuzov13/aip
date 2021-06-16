b7bfb28a35f26acf8a9afdfcaa4e36fb74223f5c
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 20:11:44 +0300

    Add files via upload

---
+++ week1/dz1#2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float p = 0;
+	float s = 0;
+	int a = 0;
+	int b = 0;
+
+	cin >> a; //caanu aai?o ?aaeon
+	cin >> b;
+
+	p = a * 2 + b * 2; // ?an??o aeeiu 
+	s = a*b; // ?an??o ieiuaae 
+
+	cout << "perimetr=" << p << endl; // auaia aeeiu
+	cout << "ploshad`=" << s << endl; // auaia ieiuaae
+
+	return 0;
+
+}

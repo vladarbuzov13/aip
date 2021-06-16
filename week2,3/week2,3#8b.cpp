630cc6ccd891574d49a015298b18bd49514fac43
vladarbuzov13 <86004070+vladarbuzov13@users.noreply.github.com>
2021-06-16 20:41:07 +0300

    Add files via upload

---
+++ week2,3/week2,3#8b.cpp
@@ -0,0 +1,30 @@
+﻿/* Задание 4.
+Вычислите значение выражения: |x| + x^5 , если x=−2. Ответ: -30*/
+
+#include <iostream>
+#include "cmath"
+#include "fstream"
+
+using namespace std;
+
+float x = -2;
+float sum;
+
+float summa(float a);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    ofstream f;
+    f.open("Task_4.txt");
+
+    f << "x = " << x << endl;
+    f << "Вычисленная сумма = " << summa(x);
+
+    f.close();
+}
+
+float summa(float x) {
+    sum = fabs(x) + pow(x, 5);
+    return (sum);
+}
